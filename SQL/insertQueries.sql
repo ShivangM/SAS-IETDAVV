5f916794be3bbcb5832da71eb0dd0ed8388b479b
ShivangM <shivangmishra0824@gmail.com>
2023-03-07 18:39:14 +0530

    Fix

---
+++ SQL/insertQueries.sql
@@ -0,0 +1,9 @@
+-- INSERT QUERIES 
+INSERT INTO students VALUES('20bit056@ietdavv.edu.in', 'Shivang Mishra', 'DE20415', '20I6056');
+INSERT INTO teacher VALUES('shivangmishra0824@gmail.com', 'Shivang Mishra');
+INSERT INTO teaches VALUES('teacher@gmail.com', 'AIR4C1');
+INSERT INTO attendance VALUES('AIR4C1','20I4056','Present');
+INSERT INTO subject VALUES('Numerical & Optimization Techniques','AIR4C1','PC');
+INSERT INTO class VALUES('20I4056', 4, 'IT', 'A');
+INSERT INTO takes VALUES(4,'IT','AIR4C1');
+INSERT INTO takes_class VALUES('teacher@gmail.com', 4, 'IT', 'A');
